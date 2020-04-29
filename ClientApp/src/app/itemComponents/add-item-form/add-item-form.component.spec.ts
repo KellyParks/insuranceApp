f602632fa82619ce96a310d4cca2d57e3fb967cc
Kelly <kellyparks111@gmail.com>
2020-04-29 14:13:01 -0600

    minor styling

--- ClientApp/src/app/itemComponents/add-item-form/add-item-form.component.spec.ts
+++ ClientApp/src/app/itemComponents/add-item-form/add-item-form.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddItemFormComponent } from './add-item-form.component';
+import { ItemAPIService } from '../../item-api.service';
 
 describe('AddItemFormComponent', () => {
   let component: AddItemFormComponent;
+  let itemApiService: ItemAPIService;
   let fixture: ComponentFixture<AddItemFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AddItemFormComponent]
+      declarations: [AddItemFormComponent],
+      providers: [ItemAPIService],
     })
       .compileComponents();
   }));
@@ -16,10 +19,22 @@ describe('AddItemFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AddItemFormComponent);
     component = fixture.componentInstance;
+    itemApiService = TestBed.get(ItemAPIService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should save a new item, and update the list', () => {
+    let newItem = {
+      name: 'Computer',
+      value: 1000,
+      category: 'Electronics'
+    };
+    component.onSubmit();
+    expect(itemApiService.createItem).toHaveBeenCalledWith(newItem);
     expect(component).toBeTruthy();
   });
 });
--- ClientApp/src/app/itemComponents/item-information/item-information.component.spec.ts
+++ ClientApp/src/app/itemComponents/item-information/item-information.component.spec.ts
@@ -22,4 +22,65 @@ describe('TestComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should delete an item', () => {
+    const id = 2;
+    component.onRemove(id);
+    expect(itemService.deleteItem).toHaveBeenCalledWith(id);
+    expect(component).toBeTruthy();
+  });
+
+  it('should get the total value of all items', () => {
+    const items = [
+      {
+        id: 2,
+        name: 'Phone',
+        value: 100,
+        category: 'Electronics'
+      },
+      {
+        id: 4,
+        name: 'Camera',
+        value: 100,
+        category: 'Electronics'
+      },
+      {
+        id: 5,
+        name: 'Shirts',
+        value: 50,
+        category: 'Clothing'
+      },
+    ];
+    const result = component.getTotalValue();
+    expect(result).toEqual(250);
+    expect(calculationService.getTotalValueOfAllItems()).toHaveBeenCalledWith(items);
+    expect(component).toBeTruthy();
+  });
+
+  it('should get the total value of items for a given category', () => {
+    const items = [
+      {
+        id: 2,
+        name: 'Phone',
+        value: 100,
+        category: 'Electronics'
+      },
+      {
+        id: 4,
+        name: 'Camera',
+        value: 100,
+        category: 'Electronics'
+      },
+      {
+        id: 5,
+        name: 'Shirts',
+        value: 50,
+        category: 'Clothing'
+      },
+    ];
+    const result = component.getTotalCategoryValue('Electronics');
+    expect(calculationService.getValueOfItemsInCategory()).toHaveBeenCalledWith('Electronics', items);
+    expect(component).toBeTruthy();
+    expect(result).toEqual(200);
+  });
 });
